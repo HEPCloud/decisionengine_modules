5ed864a7fb060a1c980d14af145e7b8b743bc9a5
Dmitry Litvinsev <litvinse@fermicloud140.fnal.gov>
2017-04-12 14:01:38 -0500

    added abstratc module

---
+++ framework/modules/Module.py
@@ -0,0 +1,14 @@
+import abc 
+
+class Module:
+
+    def __init__(self,set_of_parameters, my_data_block):
+        self.parameters = set_of_parameters
+        self.data_block = my_data_block
+
+    def get_paramaters(self):
+        return self.parameters
+
+    def get_data_bock(self):
+        return self.data_block
+        
