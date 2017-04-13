4ac2f4641015d082d3fd91e2ac3cbb88074610cd
Anthony Tiradani <anthony.tiradani@gmail.com>
2017-04-13 12:03:55 -0500

    new source for provisioner, changed htcondor q to convert data to pandas style data

--- testcases/AWS/source_htcondor_job_q.py
+++ testcases/AWS/source_htcondor_job_q.py
@@ -1,13 +1,28 @@
+import numpy as np
+import pandas as pd
+
 class HTCondorJobQ(Source):
-    PRODUCES = []
+    PRODUCES = ["job_manifests"]
 
     def __init__ (self, *args, **kwargs):
-        self.DataBlock = kwargs["DataBlock"]
         self.desired_attrs = kwargs["desired_attrs"]
         self.constraint = kwargs["constraint"]
         self.schedd_name = kwargs["schedd_name"]
 
     def produces(self): return PRODUCES
 
-    def acquire(self):
-        self.DataBlock.put("stuff")
+    # The DataBlock given to the source is t=0
+    def acquire(self, DataBlock):
+        job_manifests = [
+            {"ProcId": 1, "ClusterId": 0, "CPUS": 2, "MEM": 4, "MEM_UNITS": "GB",},
+            {"ProcId": 2, "ClusterId": 0, "CPUS": 2, "MEM": 4, "MEM_UNITS": "GB",},
+            {"ProcId": 3, "ClusterId": 0, "CPUS": 2, "MEM": 4, "MEM_UNITS": "GB",},
+            {"ProcId": 3, "ClusterId": 1, "CPUS": 2, "MEM": 4, "MEM_UNITS": "GB",},
+            {"ProcId": 3, "ClusterId": 2, "CPUS": 2, "MEM": 4, "MEM_UNITS": "GB",},
+            {"ProcId": 6, "ClusterId": 0, "CPUS": 2, "MEM": 4, "MEM_UNITS": "MB",}
+        ]
+        manifest_keys = job_manifests[0].keys()
+        pandas_data = {}
+        for key in manifest_keys:
+            pandas_data[key] = pd.Series([d[key] for d in job_manifests])
+        DataBlock["job_manifests"] = pandas_data
---
+++ testcases/AWS/source_provisioner_resources.py
@@ -0,0 +1,25 @@
+import numpy as np
+import pandas as pd
+
+class ProvisionerResourceList(Source):
+    PRODUCES = ["provisioner_resources"]
+
+    def __init__ (self, *args, **kwargs):
+        pass
+
+    def produces(self): return PRODUCES
+
+    # The DataBlock given to the source is t=0
+    def acquire(self, DataBlock):
+        resource_list = [
+            {"NAME": "AWS1", "CPUS": 1, "MEMORY": 2, "MEMORY_UNIT": "GB"},
+            {"NAME": "AWS2", "CPUS": 2, "MEMORY": 4, "MEMORY_UNIT": "GB"},
+            {"NAME": "AWS3", "CPUS": 2, "MEMORY": 6, "MEMORY_UNIT": "GB"},
+            {"NAME": "AWS4", "CPUS": 1, "MEMORY": 6, "MEMORY_UNIT": "GB"},
+            {"NAME": "AWS5", "CPUS": 2, "MEMORY": 5, "MEMORY_UNIT": "GB"}
+        ]
+        resource_keys = resource_list[0].keys()
+        pandas_data = {}
+        for key in resource_keys:
+            pandas_data[key] = pd.Series([d[key] for d in resource_list])
+        DataBlock["provisioner_resources"] = pandas_data
