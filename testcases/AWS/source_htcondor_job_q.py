714b092dc47a39adc05ba5a41bced7a75713fa5e
Anthony Tiradani <anthony.tiradani@gmail.com>
2017-04-13 13:56:24 -0500

    want ProcId and ClusterId combined for the job id

--- testcases/AWS/source_htcondor_job_q.py
+++ testcases/AWS/source_htcondor_job_q.py
@@ -4,22 +4,22 @@ import pandas as pd
 class HTCondorJobQ(Source):
     PRODUCES = ["job_manifests"]
 
-    def __init__ (self, *args, **kwargs):
-        self.desired_attrs = kwargs["desired_attrs"]
-        self.constraint = kwargs["constraint"]
-        self.schedd_name = kwargs["schedd_name"]
+    def __init__ (self, params_dict):
+        self.desired_attrs = params_dict["desired_attrs"]
+        self.constraint = params_dict["constraint"]
+        self.schedd_name = params_dict["schedd_name"]
 
     def produces(self): return PRODUCES
 
     # The DataBlock given to the source is t=0
     def acquire(self, DataBlock):
         job_manifests = [
-            {"ProcId": 1, "ClusterId": 0, "CPUS": 2, "MEM": 4, "MEM_UNITS": "GB",},
-            {"ProcId": 2, "ClusterId": 0, "CPUS": 2, "MEM": 4, "MEM_UNITS": "GB",},
-            {"ProcId": 3, "ClusterId": 0, "CPUS": 2, "MEM": 4, "MEM_UNITS": "GB",},
-            {"ProcId": 3, "ClusterId": 1, "CPUS": 2, "MEM": 4, "MEM_UNITS": "GB",},
-            {"ProcId": 3, "ClusterId": 2, "CPUS": 2, "MEM": 4, "MEM_UNITS": "GB",},
-            {"ProcId": 6, "ClusterId": 0, "CPUS": 2, "MEM": 4, "MEM_UNITS": "MB",}
+            {"JobId": "1.0", "CPUS": 2, "MEM": 4, "MEM_UNITS": "GB",},
+            {"JobId": "2.0", "CPUS": 2, "MEM": 4, "MEM_UNITS": "GB",},
+            {"JobId": "3.0", "CPUS": 2, "MEM": 4, "MEM_UNITS": "GB",},
+            {"JobId": "3.1", "CPUS": 2, "MEM": 4, "MEM_UNITS": "GB",},
+            {"JobId": "3.2", "CPUS": 2, "MEM": 4, "MEM_UNITS": "GB",},
+            {"JobId": "6.0", "CPUS": 2, "MEM": 4, "MEM_UNITS": "GB",}
         ]
         manifest_keys = job_manifests[0].keys()
         pandas_data = {}
