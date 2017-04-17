55a62b22c19e77908e8a066af02d180f7a1aae58
Marc Paterno <paterno@fnal.gov>
2017-04-17 13:28:33 -0500

    New form for include statement

--- framework/logicengine/tests/test_construction.py
+++ framework/logicengine/tests/test_construction.py
@@ -1,6 +1,12 @@
-from LogicEngine import LogicEngine
+from decisionengine.framework.logicengine import LogicEngine
 import pytest
 
 def test_default_construction():
+    """LogicEngine is not default constructible."""
     with pytest.raises(TypeError):
         engine = LogicEngine()
+
+def test_wrong_configuration():
+    """LogicEngine construction requires rules and facts."""
+    with pytest.raises(TypeError):
+        engine = LogicEngine({})
