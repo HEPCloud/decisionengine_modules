f8d42024edc95a87c16edd302189da138b2fc3da
Anthony Tiradani <anthony.tiradani@gmail.com>
2017-04-18 14:39:15 -0500

    prettified column names; modified to only pass the value back to the task manager not do a put

--- testcases/AWS/source_cloud_resource_spot.py
+++ testcases/AWS/source_cloud_resource_spot.py
@@ -12,14 +12,16 @@ class ProvisionerResourceList(Source):
     # The DataBlock given to the source is t=0
     def acquire(self, DataBlock):
         resource_list = [
-            {"RESOURCE_NAME": "AWS1", "SPOT": 1},
-            {"RESOURCE_NAME": "AWS2", "SPOT": 2},
-            {"RESOURCE_NAME": "AWS3", "SPOT": 2},
-            {"RESOURCE_NAME": "AWS4", "SPOT": 1},
-            {"RESOURCE_NAME": "AWS5", "SPOT": 2}
+            {"ResourceName": "AWS1", "SpotPrice": 1},
+            {"ResourceName": "AWS2", "SpotPrice": 2},
+            {"ResourceName": "AWS3", "SpotPrice": 2},
+            {"ResourceName": "AWS4", "SpotPrice": 1},
+            {"ResourceName": "AWS5", "SpotPrice": 2}
         ]
+
         resource_keys = resource_list[0].keys()
         pandas_data = {}
         for key in resource_keys:
             pandas_data[key] = pd.Series([d[key] for d in resource_list])
-        DataBlock["provisioner_resource_spot_prices"] = pd.DataFrame(pandas_data)
+
+        return { "provisioner_resource_spot_prices": pd.DataFrame(pandas_data) }
