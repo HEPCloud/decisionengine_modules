3d8e3cf29b16cfda9081830ed0f001d213ffb49d
Marc Paterno <paterno@fnal.gov>
2017-04-18 17:44:47 -0500

    Tests for LogicEngine now working, calling C++ RulesEngine

--- framework/logicengine/tests/test_simple_configuration.py
+++ framework/logicengine/tests/test_simple_configuration.py
@@ -1,20 +1,44 @@
 from decisionengine.framework.logicengine.LogicEngine import LogicEngine
 import pytest
 
-def test_simple_configuration():
+@pytest.fixture
+def myengine():
     facts = { "f1" : "val > 10" }
     rules = { "r1": {"expression" :"f1", "actions": ["a1", "a2"]} }
-    engine = LogicEngine({"facts": facts, "rules": rules})
+    return LogicEngine({"facts": facts, "rules": rules})
 
+
+def test_rule_that_fires():
     db =  {"val": 20} 
-    ef = engine.evaluate_facts(db)
-    print "Evaluted facts: ", ef
+    ef = myengine().evaluate_facts(db)
     assert ef["f1"] == True
-    actions, newfacts = engine.evaluate(db)
-    assert actions == { "r1" : ["a1", "a2"] }
-    assert newfacts == { "r1": [] }
+    result = myengine().evaluate(db)
+    assert type(result) == type({})
+    assert len(result) == 2
+    actions = result["actions"]
+    newfacts = result["newfacts"]
+    assert type(actions) == type({})
+    assert type(newfacts) == type({})
+    assert actions["r1"] ==  ["a1", "a2"]
+    assert len(actions) == 1
+    assert newfacts["r1"] == []
+    assert len(newfacts) == 1
 
-    actions, newfacts = engine.evaluate( {"val": 1} )
-    assert actions == { "r1" : [] }
-    assert newfacts == { "r1" : [] }
+
+def test_rule_that_does_not_fire():
+    """Rules that do not fire do not create entries in the returned
+    actions and newfacts.
+    """
+    db =  {"val": 3} 
+    ef = myengine().evaluate_facts(db)
+    assert ef["f1"] == False
+    result = myengine().evaluate(db)
+    assert type(result) == type({})
+    assert len(result) == 2
+    actions = result["actions"]
+    newfacts = result["newfacts"]
+    assert type(actions) == type({})
+    assert type(newfacts) == type({})
+    assert len(actions) == 0
+    assert len(newfacts) == 0
 
