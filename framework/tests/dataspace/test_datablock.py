be53dd3d1b41b9b87b5562173df7902d78dec861
W. David Dagenhart <wdd@fnal.gov>
2017-04-19 14:06:51 -0500

    Move test to tests dir and make runnable by pytest

--- framework/tests/dataspace/test_datablock.py
+++ framework/tests/dataspace/test_datablock.py
@@ -14,29 +14,17 @@ config = {
 }
 
 def are_dicts_same(x, y):
-    shared_items = set(x.items()) & set(y.items())
 
-    #print '--------------------------------'
-    #print '--------------------------------'
-    #print len(shared_items)
-    #print shared_items
-    #print '--------------------------------'
-    #print len(x.items())
-    #print x.items()
-    #print '--------------------------------'
-    #print '--------------------------------'
-    if len(shared_items) != len(x.items()):
-        return False
-    else:
-        return True
+    xitems = x.items()
+    yitems = y.items()
+    shared_items = set(xitems) & set(yitems)
+
+    return len(xitems) == len(yitems) and len(shared_items) == len(xitems)
 
 class TestDataBlock:
 
-
-
-
     def test_datablock(self):
-
+        print "Hello test_datablock"
         filename = config['dataspace']['filename']
         if os.path.exists(filename):
             os.unlink(filename)
@@ -45,10 +33,8 @@ class TestDataBlock:
 
         taskmanager_id = 'E0B9A7F5-B55E-47F6-B5EF-DCCB8B977AFE'
         generation_id = 9999
-        print 'Using taskmanager id = %s\n' % taskmanager_id
 
-        print 'creating datablock object ...\n'
-        datablock = DataBlock(dataspace, taskmanager_id, generation_id=9999)
+        datablock = DataBlock(dataspace, taskmanager_id, generation_id)
 
         timestamp = time.time()
         key = 'aKey'
@@ -61,7 +47,6 @@ class TestDataBlock:
         print 'Doing put:\nkey=%s\nvalue=%s\n\nheader=%s\n\nmetadata=%s\n\n' % (
             key, value, header, metadata)
         datablock.put(key, value, header, metadata)
-        #datablock[key] = value
 
         print 'Doing get: key=%s ...\n' % key
         db_value = datablock.get(key)
@@ -80,6 +65,10 @@ class TestDataBlock:
             print 'DICTS CONSISTENCY CHECK PASSED\n'
         else:
             print 'DICTS CONSISTENCY CHECK FAILED\n'
+        assert (are_dicts_same(value, db_value) and
+                are_dicts_same(header, db_header) and
+                are_dicts_same(metadata, db_metadata))
+
 
         # TEST: Insert new value for same key
         new_value = { "m2": "v2" }
@@ -88,6 +77,14 @@ class TestDataBlock:
         datablock.put(key, new_value, header, metadata)
         print 'Doing get: key=%s ...\n' % key
         print datablock.get(key)
+        db_new_value = datablock.get(key)
+        db_header = datablock.get_header(key)
+        db_metadata = datablock.get_metadata(key)
+        assert are_dicts_same(new_value, db_new_value)
+        assert (are_dicts_same(new_value, db_new_value) and
+                are_dicts_same(header, db_header) and
+                are_dicts_same(metadata, db_metadata))
+
 
         # TEST: Duplicate functionality
 
@@ -96,6 +93,7 @@ class TestDataBlock:
         dup_datablock = datablock.duplicate()
 
         print '---'
+        print 'datablock.generation_id = ', datablock.generation_id
         print 'Doing get: key=%s ...\n' % key
         print datablock.get(key)
         print 'Doing get_header: key=%s ...\n' % key
@@ -103,6 +101,7 @@ class TestDataBlock:
         print 'Doing get_metadata: key=%s ...\n' % key
         print datablock.get_metadata(key)
         print '---'
+        print 'dup_datablock.generation_id = ', dup_datablock.generation_id
         print 'Doing get on dup_datablock: key=%s\n' % key
         print dup_datablock.get(key)
         print 'Doing get_header on dup_datablock: key=%s ...\n' % key
@@ -112,12 +111,30 @@ class TestDataBlock:
         print '---'
 
         # TEST: Insert new value on duplicated datablock
-        new_value = { "m3": "v3" }
-        dup_datablock.put(key, new_value, dup_datablock.get_header(key), dup_datablock.get_metadata(key))
+        new_value3 = { "m3": "v3" }
+        dup_datablock.put(key, new_value3, dup_datablock.get_header(key), dup_datablock.get_metadata(key))
 
         print dup_datablock.get(key)
         print dup_datablock.get_header(key)
         print dup_datablock.get_metadata(key)
+
+        db_dup_value = dup_datablock.get(key)
+        db_dup_header = dup_datablock.get_header(key)
+        db_dup_metadata = dup_datablock.get_metadata(key)
+
+        assert (are_dicts_same(new_value3, db_dup_value) and
+                are_dicts_same(header, db_dup_header) and
+                are_dicts_same(metadata, db_dup_metadata))
+
+        metadata1 = Metadata(taskmanager_id, generation_time=timestamp, generation_id=(generation_id + 1))
+
+        db_orig_value = datablock.get(key)
+        db_orig_header = datablock.get_header(key)
+        db_orig_metadata = datablock.get_metadata(key)
+
+        assert (are_dicts_same(new_value, db_orig_value) and
+                are_dicts_same(header, db_orig_header) and
+                are_dicts_same(metadata1, db_orig_metadata))
 
         dataspace.close()
 
