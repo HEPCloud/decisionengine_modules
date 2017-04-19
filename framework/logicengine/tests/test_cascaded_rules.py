68f8f027d526cc443eedbda1f2057b1d26f84a6d
Marc Paterno <paterno@fnal.gov>
2017-04-19 14:04:10 -0500

    Testing cascaded rules

---
+++ framework/logicengine/tests/test_cascaded_rules.py
@@ -0,0 +1,37 @@
+from decisionengine.framework.logicengine.LogicEngine import LogicEngine
+import pytest
+
+@pytest.fixture
+def myengine():
+    facts = { "f1" : "val > 10" }
+    rules = {}
+    rules["r1"] = {"expression" :"f1", "actions": ["a1"], "facts": ["f2"]}
+    rules["r2"] = {"expression" :"f2", "actions": ["a2"], "facts": ["f3"]}
+    rules["r3"] = {"expression" :"f3", "facts": ["f4"]}
+    rules["r4"] = {"expression" :"f4", "actions": ["a4"]}
+    
+    return LogicEngine({"facts": facts, "rules": rules})
+
+def test_rule_that_fires():
+    db =  {"val": 20} 
+    ef = myengine().evaluate_facts(db)
+    assert ef["f1"] == True
+    result = myengine().evaluate(db)
+    assert type(result) == type({})
+    assert len(result) == 2
+    actions = result["actions"]
+    newfacts = result["newfacts"]
+    assert type(actions) == type({})
+    assert type(newfacts) == type({})
+    assert len(actions) == 4
+    assert actions["r1"] ==  ["a1"]
+    assert actions["r2"] ==  ["a2"]
+    assert actions["r3"] ==  []
+    assert actions["r4"] == ["a4"]
+
+    assert len(newfacts) == 4
+    assert newfacts["r1"] == ["f2"]
+    assert newfacts["r2"] == ["f3"]
+    assert newfacts["r3"] == ["f4"]
+    assert newfacts["r4"] == []
+
---
+++ framework/logicengine/tests/test_rule_with_negated_fact.py
@@ -0,0 +1,47 @@
+from decisionengine.framework.logicengine.LogicEngine import LogicEngine
+import pytest
+
+@pytest.fixture
+def myengine():
+    facts = { "f1" : "val > 10" }
+    rules = {}
+    rules["r1"] = {"expression" :"!f1", "actions": ["a1"]} 
+    rules["r2"] = {"expression" :"!(f1)", "actions": ["a2"]}
+    return LogicEngine({"facts": facts, "rules": rules})
+
+
+def test_rule_that_fires():
+    db =  {"val": 5} 
+    ef = myengine().evaluate_facts(db)
+    assert ef["f1"] == False
+    result = myengine().evaluate(db)
+    assert type(result) == type({})
+    assert len(result) == 2
+    actions = result["actions"]
+    newfacts = result["newfacts"]
+    assert type(actions) == type({})
+    assert type(newfacts) == type({})
+    assert actions["r1"] ==  ["a1"]
+    assert actions["r2"] ==  ["a2"]
+    assert len(actions) == 2
+    assert newfacts["r1"] == []
+    assert len(newfacts) == 2
+
+
+def test_rule_that_does_not_fire():
+    """Rules that do not fire do not create entries in the returned
+    actions and newfacts.
+    """
+    db =  {"val": 20}
+    ef = myengine().evaluate_facts(db)
+    assert ef["f1"] == True
+    result = myengine().evaluate(db)
+    assert type(result) == type({})
+    assert len(result) == 2
+    actions = result["actions"]
+    newfacts = result["newfacts"]
+    assert type(actions) == type({})
+    assert type(newfacts) == type({})
+    assert len(actions) == 0
+    assert len(newfacts) == 0
+
