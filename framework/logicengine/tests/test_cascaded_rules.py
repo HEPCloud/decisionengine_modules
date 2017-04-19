880ce03547e0d14d7c6f624fa088c1cdaedf83e5
Marc Paterno <paterno@fnal.gov>
2017-04-19 15:53:17 -0500

    Add test for false_actions

--- framework/logicengine/tests/test_cascaded_rules.py
+++ framework/logicengine/tests/test_cascaded_rules.py
@@ -8,7 +8,7 @@ def myengine():
     rules["r1"] = {"expression" :"f1", "actions": ["a1"], "facts": ["f2"]}
     rules["r2"] = {"expression" :"f2", "actions": ["a2"], "facts": ["f3"]}
     rules["r3"] = {"expression" :"f3", "facts": ["f4"]}
-    rules["r4"] = {"expression" :"f4", "actions": ["a4"]}
+    rules["r4"] = {"expression" :"f4", "actions": ["a4"], "false_actions": ["fa4"]}
     
     return LogicEngine({"facts": facts, "rules": rules})
 
@@ -33,5 +33,28 @@ def test_rule_that_fires():
     assert newfacts["r1"] == {"f2": True}
     assert newfacts["r2"] == {"f3": True}
     assert newfacts["r3"] == {"f4": True}
+    assert newfacts["r4"] == {}
+
+def test_rule_that_does_not_file():
+    db =  {"val": 5}
+    ef = myengine().evaluate_facts(db)
+    assert ef["f1"] == False
+    result = myengine().evaluate(db)
+    assert type(result) == type({})
+    assert len(result) == 2
+    actions = result["actions"]
+    newfacts = result["newfacts"]
+    assert type(actions) == type({})
+    assert type(newfacts) == type({})
+    assert len(actions) == 4
+    assert actions["r1"] ==  []
+    assert actions["r2"] ==  []
+    assert actions["r3"] ==  []
+    assert actions["r4"] == ["fa4"]
+
+    assert len(newfacts) == 4
+    assert newfacts["r1"] == {"f2": False}
+    assert newfacts["r2"] == {"f3": False}
+    assert newfacts["r3"] == {"f4": False}
     assert newfacts["r4"] == {}
 
