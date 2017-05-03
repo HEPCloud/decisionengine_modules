17199cdc8a14a639126e15ec1378b0a79e7e2bcf
Parag Mhashilkar <parag@fnal.gov>
2017-05-03 17:20:51 -0500

    Allow config manager to load modules outside framework directory. Needs improvements

--- framework/configmanager/ConfigManager.py
+++ framework/configmanager/ConfigManager.py
@@ -5,42 +5,45 @@ import string
 import time
 
 
-CONFIG_FILE_NAME="DE-Config.py"
+CONFIG_FILE_NAME="decision_engine.conf"
 
 class ConfigManager(object):
 
     
-    
     def __init__(self):
-        self.config_dir = os.getenv("CONFIG_PATH")
-        if not self.config_dir or not os.path.isdir(self.config_dir) : 
-            raise Exception("no config file")
-        self.config_dir += "/decisionengine"
-        self.channel_config_dir = os.path.join(self.config_dir,"config.d")
+        self.config_dir = os.getenv("CONFIG_PATH", "/etc/decisionengine")
+        if not os.path.isdir(self.config_dir): 
+            raise Exception("Config dir '%s' not found" % self.config_dir)
+        self.config_file = os.path.join(self.config_dir, CONFIG_FILE_NAME)
+        if not os.path.isfile(self.config_file): 
+            raise Exception("Config file '%s' not found" % self.config_file)
+        self.channel_config_dir = os.path.join(self.config_dir, "config.d")
 
-        self.config_file = os.path.join(self.config_dir,CONFIG_FILE_NAME)
-        
-        
+
     def load(self):
         self.last_update_time = os.stat(self.config_file).st_mtime
         code = None
-        with open(self.config_file,"r") as f:
-            code = "self.global_config="+string.join(f.readlines(),"")
-        if code : 
+        with open(self.config_file, "r") as f:
+            code = "self.global_config=" + string.join(f.readlines(), "")
+        if code: 
             exec(code)
         # load channels 
         self.channels = {}
         for direntry in os.listdir(self.channel_config_dir):
             name = direntry.split('.')[0]
-            with open(os.path.abspath(os.path.join(self.channel_config_dir,direntry)),"r") as f:
-                code="self.channels[name] ="+string.join(f.readlines(),"")
+            channel_conf = os.path.join(self.channel_config_dir, direntry)
+            with open(os.path.abspath(channel_conf), "r") as f:
+                code = "self.channels[name]=" + string.join(f.readlines(), "")
                 exec(code)
+
 
     def isUpdated():
         return 
 
+
     def get_channels(self):
         return self.channels
+
 
     def get_global_config(self):
         return self.global_config
@@ -49,20 +52,26 @@ class ConfigManager(object):
     Factory method:  create instance of dynamically loaded module 
     """
     @staticmethod
-    def create(name,parameters):
-        classname = name
+    def create(name, parameters):
+        classname = name.split('.')[-1]
+        # Channel config is required to provide with full module path
         if len(name.split('.')) > 1:
             module_name = "decisionengine.framework.%s"%(name,)
-            module = importlib.import_module(module_name)
             classname = name.split('.')[-1]
         else:
             module_name = "decisionengine.framework.modules.%s"%(name,)
+
+        try:
             module = importlib.import_module(module_name)
-        clazz  = getattr(module, classname)
-        instance  = clazz(parameters)
+        except ImportError:
+            # TODO: Modules are external to the framework. Need to handle that
+            module = importlib.import_module(name)
+
+        clazz = getattr(module, classname)
+        instance = clazz(parameters)
         return instance 
+
 
 if __name__ == "__main__":
     c = ConfigManager()
     c.load()
-            
