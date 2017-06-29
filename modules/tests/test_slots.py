052ce2b77437089536eadbacabcbd6ec49ba6ab4
Parag Mhashilkar <parag@fnal.gov>
2017-06-28 23:08:27 -0500

    Source, tests and fixture for slots aka startd classads

---
+++ modules/tests/test_slots.py
@@ -0,0 +1,35 @@
+import os
+import pytest
+import mock
+import pprint
+import utils
+from decisionengine.modules.htcondor import htcondor_query
+from decisionengine.modules.htcondor import s_slots
+
+
+config_cs = {
+    'condor_config': 'condor_config',
+    'collector_host': 'fermicloud122.fnal.gov',
+}
+
+
+class TestStartdManifests:
+
+    def test_produces(self):
+        produces = ['startd_manifests']
+        slots = s_slots.StartdManifests(config_cs)
+        assert(slots.produces() == produces)
+
+
+    def test_acquire(self):
+        slots = s_slots.StartdManifests(config_cs)
+        with mock.patch.object(htcondor_query.CondorStatus, 'fetch') as f:
+            f.return_value = utils.input_from_file('cs.fixture')
+            pprint.pprint(slots.acquire())
+    """
+
+
+    def test_acquire_live(self):
+        slots = s_slots.StartdManifests(config_cs)
+        pprint.pprint(slots.acquire())
+    """
