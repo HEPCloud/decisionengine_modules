9f15b37ae6f67c81f8cbf60354aba174764a2f49
Parag Mhashilkar <parag@fnal.gov>
2017-06-28 23:06:20 -0500

    Source, tests and fixture for factory_client classads

---
+++ modules/tests/test_factory_client.py
@@ -0,0 +1,34 @@
+import os
+import pytest
+import mock
+import pprint
+import utils
+from decisionengine.modules.glideinwms import s_factory_client
+from decisionengine.modules.htcondor import htcondor_query
+
+
+config = {
+    'condor_config': 'condor_config',
+    'collector_host': 'fermicloud122.fnal.gov:8618',
+    #'classad_attrs': ['Name', 'EntryName', 'GLIDEIN_Gatekeeper', 'GLIDEIN_GridType'],
+}
+
+
+class TestFactoryClientManifests:
+
+    def test_produces(self):
+        factory_client = s_factory_client.FactoryClientManifests(config)
+        produces = ['factoryclient_manifests']
+        assert(factory_client.produces() == produces)
+
+
+    def test_acquire(self):
+        factory_client = s_factory_client.FactoryClientManifests(config)
+        with mock.patch.object(htcondor_query.CondorStatus, 'fetch') as f:
+            f.return_value = utils.input_from_file('factory_client.cs.fixture')
+            pprint.pprint(factory_client.acquire())
+
+
+    def test_acquire_live(self):
+        factory_client = s_factory_client.FactoryClientManifests(config)
+        pprint.pprint(factory_client.acquire())
