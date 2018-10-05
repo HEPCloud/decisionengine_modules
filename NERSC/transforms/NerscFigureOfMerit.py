5e5dba5a1684998a3111378cd6afa53274f4821c
Dmitry Litvintsev <litvinse@fnal.gov>
2018-10-05 15:46:25 -0500

    adapt to "EntryName" in CSV

--- NERSC/transforms/NerscFigureOfMerit.py
+++ NERSC/transforms/NerscFigureOfMerit.py
@@ -15,7 +15,7 @@ from decisionengine.framework.modules import Transform
 import decisionengine.framework.modules.de_logger as de_logger
 
 """
-IMPORTANT: Please do not change order of these keys and always 
+IMPORTANT: Please do not change order of these keys and always
            append new keys rather than pre-pend or insert.
 """
 CONSUMES = ["Nersc_Instance_Performance", "Factory_Entries_LCF"]
@@ -31,49 +31,24 @@ class NerscFigureOfMerit(Transform.Transform):
 
     def transform(self, data_block):
         """
-        NERS Instance performance is obtained from the following CSV:
+        NERSC Instance performance is obtained from the following CSV:
 
-        InstanceType,AvailabilityZone,OnDemandPrice,PerfTtbarTotal
-        haswell,cori,0.576,0.96
-        haswell_shared,cori,0.081,0.137
-        knl,cori,0.612,0.255
-        haswell,edison,0.432,0.658
+        InstanceType,AvailabilityZone,OnDemandPrice,PerfTtbarTotal,EntryName
+        haswell,cori,0.576,0.96,CMSHTPC_T3_US_NERSC_Cori
+        haswell_shared,cori,0.081,0.137,CMSHTPC_T3_US_NERSC_Cori_shared
+        knl,cori,0.612,0.255,CMSHTPC_T3_US_NERSC_Cori_KNL
+        haswell,edison,0.432,0.658,CMSHTPC_T3_US_NERSC_Edison
+        haswell_shared,edison,0.108,0.164,CMSHTPC_T3_US_NERSC,Edison_shared
 
         by NerscInstancePerformance
 
-        first column is HW type, second the machine, third the \"price\" 
-        of what 1 hr of that resource is worth 
+        first column is HW type, second the machine, third the \"price\"
+        of what 1 hr of that resource is worth
 
-        4th column is the cms ttbar benchmark 
+        4th column is the cms ttbar benchmark
 
-        So therefore price/performance is the ratio of the 3rd and 4th column
+        price/performance is the ratio of the 3rd and 4th column
 
-        Factory_Entries_LCF data frame contains EntryName column 
-        which is defined in the following way:
-
-        EntryName=${VO_PREFIX}_${SITE_PREFIX}_${Avalability_Zone}_${Instance_Type}
-        
-        VO_PREFIX = ['CMSHTPC', 'FIFE']
-        SITE_PREFIX = T3_US_NERSC
-        Avalability_Zone = ['Cori', 'Edison'] (matches AvailabilityZone from Nersc_Instance_Performance)
-        Instance_Type = ['KNL','Edison'] (matches InstanceType from Nersc_Instance_Performance)
-
-        Above mapping is captured in configuration of this 
-        modulde under "entry_name_mapping" key
-        
-        user mapping (for the future):
-
-        hufnagel-> cms   timm-> mu2e  burt -> nova 
-
-        mapping: 
-
-         InstanceType,AvailabilityZone        EntryName
-         haswell,cori                         CMSHTPC_T3_US_NERSC_Cori
-         haswell_shared,cori                  CMSHTPC_T3_US_NERSC_Cori_shared
-         knl,cori,0.612,0.255                 CMSHTPC_T3_US_NERSC_Cori_KNL
-         haswell,edison,0.432,0.658           CMSHTPC_T3_US_NERSC_Edison
-         
-         For FIFE substitute FIFE for CMSHTPC above
         """
 
         performance = data_block[CONSUMES[0]]
@@ -83,23 +58,10 @@ class NerscFigureOfMerit(Transform.Transform):
 
         factory_entries_lcf = data_block[CONSUMES[1]]
 
-        try:
-            entry_name_mapping = self.config["entry_name_mapping"]
-        except KeyError:
-            raise RuntimeError("Transform {} is misconfigured. Configuration is missing \"entry_name_mapping\" key.".
-                                   format(self.__class__.__name__))
-
         figures_of_merit = []
         for i, row in factory_entries_lcf.iterrows():
             entry_name = row["EntryName"]
-            key = re.sub("^[^_]*_", "", entry_name)
-            try:
-                instance_type, availability_zone = entry_name_mapping.get(key).split(",")
-            except AttributeError:
-                raise RuntimeError("Transform {} is misconfigured. Configuration \"entry_name_mapping\" is missing key = {}.".
-                                   format(self.__class__.__name__, key))
-            perf_df = performance[(performance.InstanceType == instance_type) &
-                                  (performance.AvailabilityZone == availability_zone)]
+            perf_df = performance[performance.EntryName == entry_name]
 
             for j, perf_row in perf_df.iterrows():
                 running = float(row["GlideinMonitorTotalStatusRunning"])
@@ -131,9 +93,9 @@ def module_config_template():
            "module":  "modules.AWS.transforms.NerscFigureOfMerit",
            "name":  "NerscFigureOfMerit",
            "parameters": {
-               "entry_name_mapping" : { 
+               "entry_name_mapping" : {
                    "T3_US_NERSC_Cori" : "haswell,cori",
-                   "T3_US_NERSC_Cori_shared" : "haswell_shared,cori", 
+                   "T3_US_NERSC_Cori_shared" : "haswell_shared,cori",
                    "T3_US_NERSC_Cori_KNL" : "knl,cori",
                    "T3_US_NERSC_Edison" : "haswell,edison",
                },
