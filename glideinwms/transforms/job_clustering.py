6ea048510b8f9fb33fbbec48eb2ab93b3cec4156
Krista Majewski <klarson1@fnal.gov>
2018-10-22 09:27:54 -0500

    Job cluster refactor

--- glideinwms/transforms/job_clustering.py
+++ glideinwms/transforms/job_clustering.py
@@ -16,15 +16,6 @@ CONSUMES = ['job_manifests']
 # Expects a named list of expressions
 # Expects a list of expressions to apply for each category type, 
 # each category corresponds to a job classad attr and expressions to apply to it for buckets 
-DEFAULT_JOB_CATEGORIES = [
-  #[ "VO_Name=='cms'", "VO_Name=='des'", "VO_Name=='nova'" ],
-  ["RequestCPUs==1", "RequestCPUs==2", "RequestCPUs=4"],
-  #[ "MaxWallTimeMins>0 && MaxWallTimeMins<= 60*12", "MaxWallTimeMins>60*12 && MaxWallTimeMins<= 60*24", "MaxWallTimeMins>60*24 && MaxWallTimeMins<= 60*12*2", "MaxWallTimeMins>60*24*2 && MaxWallTimeMins<= 60*12*4" ],
-  #[ "RequestMemory==??" ],
-  #[ "Require_CVMFS==True", "Require_CVMFS==False" ],
-  #[ "Input_IO_Rate ==??" ], 
-  #[ "Data_Location=='fnal'", "Data_Location=='aaa'", "Data_Location=='cloud'", "Data_Location=='local'" ]
-]
 
 # TODO 
 # - what debugging logs are needed?
@@ -39,7 +30,8 @@ class JobClustering(Transform.Transform):
         if not isinstance(config, dict):
             raise RuntimeError('parameters for module config should be a dict')
 
-        self.job_categories = config.get('job_categories')
+        self.match_exprs = config.get('match_expressions')
+        self.job_q_expr = config.get('job_q_expr')
 
         self.logger = de_logger.get_logger()
 
@@ -65,49 +57,31 @@ class JobClustering(Transform.Transform):
 
         # Get job queue datablock
         try:
-            df_q = datablock.get('job_manifests')
+            df_full_q = datablock.get('job_manifests')
+            df_q = df_full_q.query(self.job_q_expr)
         except KeyError, ValueError:
             self.logger.error("Unable to retrieve job manifests data block")
             return pandas.DataFrame({
-                'Bucket_Criteria_Expr': [""],
+                'Job_Bucket_Criteria_Expr': [""],
+                'Site_Bucket_Criteria_Expr': [""],
                 'Totals': [0]
                 },
-                columns=['Bucket_Criteria_Expr', 'Totals'])
-            
+                columns=['Job_Bucket_Criteria_Expr', 'Site_Bucket_Criteria_Expr', 'Totals'])
 
-        # Read in category configs
-        if self.job_categories:
-            job_category_config = self.job_categories
-            self.logger.info("Using config job categories")
-        else:
-            job_category_config = DEFAULT_JOB_CATEGORIES
-            self.logger.info("Using DEFAULT job categories")
-
-        # Create clusters
-        buckets = list(itertools.product(*job_category_config))
-
-        # Append tuple into single query string
-        bucket_list = [" and ".join(x) for x in buckets]
-        self.logger.debug("Buckets created: %s" % bucket_list)
-
-        # Create bucket dataframe and extend with totals
-        df_job_clusters = pandas.DataFrame(bucket_list, columns=["Bucket_Criteria_Expr"])
-        df_job_clusters['Totals'] = pandas.Series([0]*len(bucket_list))
-
+        totals = []
         try:
             # Query job q and populate bucket totals
-            for b_l in bucket_list:
-                # Get index
-                buckets_idx = df_job_clusters.index[df_job_clusters["Bucket_Criteria_Expr"] == b_l].tolist()[0]
-                df_job_clusters.iloc[buckets_idx,1] = df_q.query(b_l).shape[0]
-            self.logger.debug("Job category totals:\n" % df_job_clusters)
+            totals = [[job_expr, self.match_exprs.get(job_expr), df_q.query(job_expr).shape[0]] for job_expr in self.match_exprs.keys()]
+            df_job_clusters = pandas.DataFrame(totals, columns=['Job_Bucket_Criteria_Expr', 'Site_Bucket_Criteria_Expr', 'Totals'])
+            self.logger.debug("Job category totals: %s" % df_job_clusters)
         except KeyError, ValueError:
             self.logger.error("Unable to calculate totals from job manifests, may have missing classads or incorrect classad names")
             return pandas.DataFrame({
-                'Bucket_Criteria_Expr': [""],
+                'Job_Bucket_Criteria_Expr': [""],
+                'Site_Bucket_Criteria_Expr': [""],
                 'Totals': [0]
                 },
-                columns=['Bucket_Criteria_Expr', 'Totals'])
+                columns=['Job_Bucket_Criteria_Expr', 'Site_Bucket_Criteria_Expr', 'Totals'])
 
         self.logger.info("*** Ending job clustering ***")
 
@@ -124,11 +98,9 @@ def module_config_template():
             'module': 'decisionengine.modules.glideinwms.t_job_clustering',
             'name': 'JobClustering',
             'parameters': {
-                'job_categories': [
-                   ["VO_Name=='cms'", "VO_Name=='des'", "VO_Name=='nova'"],
-                   ["RequestCpus==1", "RequestCpus==2", "RequestCpus==4"],
-                   ["(MaxWallTimeMins>0 && MaxWallTimeMins<= 60*12)", "(MaxWallTimeMins>60*12 && MaxWallTimeMins<= 60*24)", "(MaxWallTimeMins>60*24 && MaxWallTimeMins<= 60*12*2)"]
-                 ]
+                'match_expressions': {
+                      "VO_Name=='cms' and RequestCpus==1 and (MaxWallTimeMins>0 and MaxWallTimeMins<= 60*12)": ["GLIDEIN_Supported_VOs.str.contains('CMS') and GLIDEIN_CPUS == 1"]
+                 }
             },
         }
     }
