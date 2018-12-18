ae36f63cc12c03861642bc44e5a23f048be067e7
Dmitry Litvintsev <litvinse@fnal.gov>
2018-12-18 15:10:05 -0600

    use >= instead of ==

--- util/figure_of_merit.py
+++ util/figure_of_merit.py
@@ -9,8 +9,8 @@ _INFINITY = sys.float_info.max
 def figure_of_merit(performance,
                     running,
                     allowed):
-    if running == allowed:
+    if running >= allowed:
         return _INFINITY
-    return float(performance)*float(running+1)/float(allowed)
+    return performance*float(running+1)/allowed
 
 
