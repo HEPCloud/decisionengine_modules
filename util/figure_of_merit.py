4b97c48086fcbd74bf206a3c7745821223a87346
Dmitry Litvintsev <litvinse@fnal.gov>
2019-01-11 11:34:46 -0600

    Modified figure of merit formula to include idle and max_idle constraint. Use the formula in Ners figure of merit transform

--- util/figure_of_merit.py
+++ util/figure_of_merit.py
@@ -8,9 +8,14 @@ _INFINITY = sys.float_info.max
 
 def figure_of_merit(performance,
                     running,
-                    allowed):
+                    allowed,
+                    idle=None,
+                    max_idle=None):
     if running >= allowed:
         return _INFINITY
+    if idle is not None and max_idle is not None:
+        if idle >= max_idle:
+            return _INFINITY
     return performance*float(running+1)/allowed
 
 
