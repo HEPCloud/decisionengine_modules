44b41f5ea8cd975e7e355e43e873d680790e81a6
Parag Mhashilkar <parag@fnal.gov>
2019-01-28 21:16:13 -0600

    Request idle glideins to resources based on the facts set by the logic engine. Expects fact names to be allow_grid_requests, allow_lcf_requests, allow_aws_requests, allow_gce_req

--- glideinwms/publishers/fe_group_classads.py
+++ glideinwms/publishers/fe_group_classads.py
@@ -4,19 +4,28 @@ import pprint
 import pandas
 import numpy
 
+from decisionengine.framework.modules import de_logger
 from decisionengine_modules.htcondor.publishers import publisher
 
+# TODO: Enable publishing of glideresource_manifests
+#'glideclient_manifests', 'glideresource_manifests',
 CONSUMES = [
-    # TODO: Enable publishing of glideresource_manifests
-    #'glideclient_manifests', 'glideresource_manifests',
-    'glideclient_manifests',
+    'glideclient_manifests', 'Factory_Entries_Grid', 'Factory_Entries_AWS', 'Factory_Entries_GCE', 'Factory_Entries_LCF'
 ]
 
 
 class GlideinWMSManifests(publisher.HTCondorManifests):
 
+
     def __init__(self, *args, **kwargs):
         super(GlideinWMSManifests, self).__init__(*args, **kwargs)
+        self.logger = de_logger.get_logger()
+        self._fact_entrytype_map = {
+            'allow_grid_requests': 'Factory_Entries_Grid',
+            'allow_aws_requests': 'Factory_Entries_AWS',
+            'allow_gce_requests': 'Factory_Entries_GCE',
+            'allow_lcf_requests': 'Factory_Entries_LCF'
+        }
 
 
     def consumes(self):
@@ -24,6 +33,43 @@ class GlideinWMSManifests(publisher.HTCondorManifests):
         Return list of items produced
         """
         return CONSUMES
+
+
+    def publish(self, datablock):
+        requests_df = datablock.get('glideclient_manifests')
+        facts_df = datablock.get('de_logicengine_facts')
+
+        # Create a map of allow facts & entries
+        allow_type_entries = {i: datablock.get(self._fact_entrytype_map[i]) for i in self._fact_entrytype_map}
+
+        # Initialize to empty dataframes and add row at a time
+        allow_type_req_dfs = {i: pandas.DataFrame() for i in self._fact_entrytype_map}
+        for index, row in requests_df.iterrows():
+            # Iterate through all requests one at a time
+            # Check if the ReqName coincides with the allowed request types
+            req_name = row['ReqName']
+
+            # Identify the type of request ie (grid|gce|aws|lcf))
+            for allow_type in allow_type_entries:
+                these_entries = allow_type_entries[allow_type]
+                if len(these_entries.query('Name=="%s"' % req_name)) > 0:
+                    # This request belongs to allowed_type
+                    allow_type_req_dfs[allow_type] = allow_type_req_dfs[allow_type].append(row)
+                    break
+
+        self.logger.info('Facts available in publisher %s: %s' % (self.__class__.__name__, facts_df.to_dict(orient='records')))
+        for index, row in facts_df.iterrows():
+            fact_name = row['fact_name']
+            fact_value = ('%s' % row['fact_value']).lower() == 'true'
+            if not fact_value:
+                # Convert request idle to 0
+                # TODO: Check what to do with max running
+                #       For now keep it same so existing glideins can finish
+                self.logger.info('Setting ReqIdleGlideins=0 for fact: %s' % fact_name)
+                allow_type_req_dfs[fact_name]['ReqIdleGlideins'] = [0] * len(allow_type_req_dfs[fact_name])
+
+        publish_requests_df = pandas.DataFrame(pandas.concat(allow_type_req_dfs.values(), ignore_index=True))
+        self.publish_to_htcondor(publish_requests_df)
 
 
 def module_config_template():
