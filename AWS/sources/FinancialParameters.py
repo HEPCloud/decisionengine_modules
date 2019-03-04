c567eeee893026594772c91d906c08c004412bd1
Steven Timm <timm@fnal.gov>
2019-03-04 11:06:33 -0600

    changed the calling structure to return the right return type

--- AWS/sources/FinancialParameters.py
+++ AWS/sources/FinancialParameters.py
@@ -11,15 +11,17 @@ from decisionengine.framework.modules import Source
 PRODUCES = ["financial_params"]
 
 class FinancialParameters(Source.Source):
-    def __init__ (self, params_dict):
-        pass
+    def __init__ (self, config):
+        super(FinancialParameters, self).__init__(config)
+        self.financial_parameters_dict = config.get('financial_parameters')
+
 
     def produces(self,schema_id_list): return PRODUCES
 
     # The DataBlock given to the source is t=0
-    def acquire(self,params_dict):
-	financial_params=pandas.DataFrame(params_dict)
-        return financial_params
+    def acquire(self):
+	financial_params=pandas.DataFrame(self.financial_parameters_dict)
+        return {PRODUCES[0]: financial_params}
 
 def module_config_template():
     """
@@ -29,12 +31,14 @@ def module_config_template():
     d = {"FinancialParameters": {
         "module" :  "modules.AWS.sources.FinancialParameters",
         "name"   :  "FinancialParameters",
-                    "parameters": {
+                    "parameters": { 
+			"financial_parameters": {
                         "target_aws_vm_burn_rate":     [ 9.0 ],
                         "target_aws_bill_burn_rate":  [ 10.0 ],
                         "target_aws_balance":       [ 1000.0 ],
                         "target_gce_vm_burn_rate":   [   9.0 ],
                         "target_gce_balance":       [ 1000.0 ],
+                        }
                     }
         },
         "schedule": 60*60,
@@ -75,15 +79,16 @@ def main():
         module_config_info()
     else:
         myparams= {
-            "target_aws_vm_burn_rate":     [ 9.0 ],
-            "target_aws_bill_burn_rate":  [ 10.0 ],
-            "target_aws_balance":       [ 1000.0 ],
-            "target_gce_vm_burn_rate":   [   9.0 ],
-            "target_gce_balance":       [ 1000.0 ],
+                   "financial_parameters": {
+            		"target_aws_vm_burn_rate":     [ 9.0 ],
+            		"target_aws_bill_burn_rate":  [ 10.0 ],
+            		"target_aws_balance":       [ 1000.0 ],
+            		"target_gce_vm_burn_rate":   [   9.0 ],
+            		"target_gce_balance":       [ 1000.0 ],
+                        }
         }
         MyFP=FinancialParameters(myparams)
-        mydf=MyFP.acquire(myparams)
-        print mydf     
+        mydf=MyFP.acquire()
 
 if __name__ == "__main__":
     main()
