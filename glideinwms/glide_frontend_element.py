480c1c82c4152335ef73d4bc3037e1d3631f7665
Parag Mhashilkar <parag@fnal.gov>
2019-05-03 11:26:01 -0500

    cleanup commented code

--- glideinwms/glide_frontend_element.py
+++ glideinwms/glide_frontend_element.py
@@ -1428,7 +1428,6 @@ class GlideFrontendElementFOM(GlideFrontendElement):
             ent_list = entry_info.get('EntryName', []).tolist()
             ent_name = ent_list[0] if ent_list else "-"
             fom_info = self.fom_entries.query('EntryName=="%s"' % ent_name)
-            #fom = fom_info.get('FOM').tolist()[0]
             try:
                 fom = fom_info.get('FOM').tolist()[0]
             except IndexError:
