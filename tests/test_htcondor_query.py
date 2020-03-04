f1cb786e58551408511c6178a050614ad5bde110
Dmitry Litvintsev <litvinse@fnal.gov>
2020-03-04 13:23:41 -0600

    make htcondor_query test pass

--- tests/test_htcondor_query.py
+++ tests/test_htcondor_query.py
@@ -1,10 +1,9 @@
 import os
-import pytest
+
 import mock
-import pprint
-import classad
-from decisionengine_modules.util import testutils as utils
+
 from decisionengine_modules.htcondor import htcondor_query
+from decisionengine_modules.util import testutils as utils
 
 DATA_DIR = os.path.join(os.path.dirname(__file__), "data")
 FIXTURE_FILE = os.path.join(DATA_DIR, "cs.fixture")
@@ -31,88 +30,46 @@ config_cs_bad = {
     'pool_name': 'dummy_collector.fnal.gov, bad_collector.fnal.gov',
 }
 
+
 class TestCondorQ:
-
-    def test_condorq_live(self):
-        assert False
-#        if not os.environ.get('CONDOR_CONFIG'):
-#            os.environ['CONDOR_CONFIG'] = config_cq.get('condor_config')
-#
-#        condor_q = htcondor_query.CondorQ(
-#            schedd_name=config_cq.get('schedd_name'),
-#            pool_name=config_cq.get('pool_name'))
-#
-#        condor_q.load(constraint='procid < 2')
-#        #pprint.pprint(condor_q.stored_data)
-#        pprint.pprint('Fetched %s job classads from schedd %s in collector %s' % (len(condor_q.stored_data), config_cq.get('schedd_name'), config_cq.get('pool_name')))
-#
-
     def test_condorq_queryerror(self):
-        assert False
-#        if not os.environ.get('CONDOR_CONFIG'):
-#            os.environ['CONDOR_CONFIG'] = config_cq_bad.get('condor_config')
-#
-#        condor_q = htcondor_query.CondorQ(
-#            schedd_name=config_cq_bad.get('schedd_name'),
-#            pool_name=config_cq_bad.get('pool_name'))
-#        try:
-#            condor_q.load(constraint='procid < 2')
-#            assert False
-#        except htcondor_query.QueryError:
-#            assert True
-#        #pprint.pprint(condor_q.stored_data)
-#
+        condor_q = htcondor_query.CondorQ(
+            schedd_name=config_cq_bad.get('schedd_name'),
+            pool_name=config_cq_bad.get('pool_name'))
+        try:
+            condor_q.load(constraint='procid < 2')
+            assert False
+        except htcondor_query.QueryError:
+            assert True
 
     def test_condorq(self):
-        assert False
+        condor_q = htcondor_query.CondorQ(
+            schedd_name=config_cq.get('schedd_name'),
+            pool_name=config_cq.get('pool_name'))
 
-#        condor_q = htcondor_query.CondorQ(
-#            schedd_name=config_cq.get('schedd_name'),
-#            pool_name=config_cq.get('pool_name'))
-#
-#        with mock.patch.object(htcondor_query.CondorQ, 'fetch') as f:
-#            f.return_value = utils.input_from_file(FIXTURE_FILE)
-#            condor_q.load()
-#            pprint.pprint('Fetched %s job classads' % len(condor_q.stored_data))
-#
+        with mock.patch.object(htcondor_query.CondorQ, 'fetch') as f:
+            f.return_value = utils.input_from_file(FIXTURE_FILE)
+            condor_q.load()
+            assert (f.return_value == condor_q.stored_data)
+
 
 class TestCondorStatus:
-
-    def test_condorstatus_live(self):
-        assert False
-#        if not os.environ.get('CONDOR_CONFIG'):
-#            os.environ['CONDOR_CONFIG'] = config_cs.get('condor_config')
-#
-#        condor_status = htcondor_query.CondorStatus(
-#            pool_name=config_cs.get('pool_name'))
-#
-#        condor_status.load()
-#        pprint.pprint('Fetched %s resource classads from collector %s' % (len(condor_status.stored_data), config_cs.get('pool_name')))
-#
-
     def test_condorstatus_queryerror(self):
-        assert False
-#        if not os.environ.get('CONDOR_CONFIG'):
-#            os.environ['CONDOR_CONFIG'] = config_cs_bad.get('condor_config')
-#
-#        condor_status = htcondor_query.CondorStatus(
-#            pool_name=config_cs_bad.get('pool_name'))
-#
-#        try:
-#            condor_status.load()
-#            assert False
-#        except htcondor_query.QueryError:
-#           assert True
-#
+        condor_status = htcondor_query.CondorStatus(
+            pool_name=config_cs_bad.get('pool_name'))
+
+        try:
+            condor_status.load()
+            assert False
+        except htcondor_query.QueryError:
+            assert True
 
     def test_condorstatus(self):
-        assert False
-#        condor_status = htcondor_query.CondorStatus(
-#            subsystem_name='any',
-#            pool_name=config_cs.get('pool_name'))
-#
-#        with mock.patch.object(htcondor_query.CondorStatus, 'fetch') as f:
-#            f.return_value = utils.input_from_file(FIXTURE_FILE)
-#            condor_status.load()
-#            #pprint.pprint(condor_status.stored_data)
-#            pprint.pprint('Fetched %s resource classads' % len(condor_status.stored_data))
+        condor_status = htcondor_query.CondorStatus(
+            subsystem_name='any',
+            pool_name=config_cs.get('pool_name'))
+
+        with mock.patch.object(htcondor_query.CondorStatus, 'fetch') as f:
+            f.return_value = utils.input_from_file(FIXTURE_FILE)
+            condor_status.load()
+            assert (f.return_value == condor_status.stored_data)
