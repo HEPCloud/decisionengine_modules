5a44018603994b64253356186c27d358961a0d2c
Patrick Gartung <gartung@fnal.gov>
2020-03-06 15:55:55 -0600

    Update decisionengine_modules rpmbuild and pylint actions. (#9)
    * Use the decisionengine_modules subdir as input to package.sh
    
    * Cache dependencies directory
    
    * Syntax error
    
    * Stop removing things that take a long time to download
    
    * Give cache dependencies its own name

--- .github/actions/rpmbuild-in-sl7-docker/entrypoint.sh
+++ .github/actions/rpmbuild-in-sl7-docker/entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -x
-decisionengine_modules/build/packaging/rpm/package.sh $PWD
+decisionengine_modules/build/packaging/rpm/package.sh decisionengine_modules
 tar cvf $GITHUB_WORKSPACE/rpmbuild.tar /var/tmp/root/rpm/decisionengine_modules/*
 exit 0
