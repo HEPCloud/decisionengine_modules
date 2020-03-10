4151a9aa084ceeb1212c11bafdce0930fac6ab83
Dmitry Litvintsev <litvinse@fnal.gov>
2020-03-10 15:12:57 -0500

    unittest: Fix GCEResourceLimits so that it does not depend on configuration (#12)
    directory and valid configuration file being present

--- tests/test_GCEResourceLimits.py
+++ tests/test_GCEResourceLimits.py
@@ -3,9 +3,11 @@ import os
 import mock
 import pandas
 
-from decisionengine_modules.util import testutils as utils
+import decisionengine.framework.configmanager.ConfigManager as configmanager
+import decisionengine.framework.dataspace.dataspace as dataspace
 from decisionengine.framework.modules import SourceProxy
 from decisionengine_modules.GCE.sources import GCEResourceLimits
+from decisionengine_modules.util import testutils as utils
 
 DATA_DIR = os.path.join(os.path.dirname(__file__), "data")
 FIXTURE_FILE = os.path.join(DATA_DIR, "gce_limits_factory_entries.test")
@@ -33,14 +35,20 @@ PRODUCES = ["GCE_Resource_Limits"]
 class TestGCEResourceLimits:
 
     def test_produces(self):
-        gce_resource_limits = GCEResourceLimits.GCEResourceLimits(CONFIG)
-        assert gce_resource_limits.produces() == PRODUCES
+        with mock.patch.object(configmanager, "ConfigManager") as config:
+            with mock.patch.object(dataspace, "DataSpace") as ds:
+                gce_resource_limits = GCEResourceLimits.GCEResourceLimits(CONFIG)
+                assert gce_resource_limits.produces() == PRODUCES
 
     def test_acquire(self):
-        gce_resource_limits = GCEResourceLimits.GCEResourceLimits(CONFIG)
-        with mock.patch.object(SourceProxy.SourceProxy, "acquire") as factory_data:
-            factory_entries = utils.input_from_file(FIXTURE_FILE)
-            factory_data.return_value = {"Factory_Entries_GCE": pandas.DataFrame(factory_entries)}
-            gce_limits = gce_resource_limits.acquire()
-            assert PRODUCES == gce_limits.keys()
-            assert CONFIG.get("entry_limit_attrs").sort() == list(gce_limits.get(PRODUCES[0])).sort()
+        with mock.patch.object(configmanager, "ConfigManager") as config:
+            with mock.patch.object(dataspace, "DataSpace") as ds:
+                with mock.patch.object(SourceProxy.SourceProxy, "acquire") as factory_data:
+                    gce_resource_limits = GCEResourceLimits.GCEResourceLimits(CONFIG)
+                    factory_entries = utils.input_from_file(FIXTURE_FILE)
+                    factory_data.return_value = {"Factory_Entries_GCE": pandas.DataFrame(factory_entries)}
+                    gce_limits = gce_resource_limits.acquire()
+                    assert PRODUCES == list(gce_limits.keys())
+                    assert CONFIG.get("entry_limit_attrs").sort() == list(gce_limits.get(PRODUCES[0])).sort()
+
+
