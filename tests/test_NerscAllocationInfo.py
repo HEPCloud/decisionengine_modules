6d8a5f5f45159c18b2b79efab7e1dcabedbe039a
shreyb <sbhat@fnal.gov>
2020-03-26 22:59:46 -0500

    decisionengine_modules/NERSC: Make Nersc API call backward-compatible with old config (#196)
    decisionengine_modules/NERSC: Make Nersc API call backward-compatible
    with old config
    
    * RB: https://fermicloud140.fnal.gov/reviews/r/87/
    * Mapped new IRIS keys to old NEWT keys were appropriate
    * Changed unit test test_NerscAllocationInfo to mock and test new API response

--- tests/test_NerscAllocationInfo.py
+++ tests/test_NerscAllocationInfo.py
@@ -11,22 +11,21 @@ DATA_DIR = os.path.join(os.path.dirname(__file__), "data")
 ALLOCATIONS_FIXTURE_FILE = os.path.join(
     DATA_DIR, "newt_allocations.cs.fixture")
 PASSWD_FILE = os.path.join(DATA_DIR, "passwd")
+FAKE_USER = "user2"
 
 CONFIG = {
     "passwd_file": PASSWD_FILE,
     "constraints": {
-        "usernames": ["timm", "user2"],
+        "usernames": ["timm", FAKE_USER],
         "newt_keys": {
             "rname": ["m2612", "m2696", "m2015"],
-            "repo_type": ["STR", ],
+            "repo_type": ["REPO", ],
         }
     }
 }
 
 PRODUCES = ["Nersc_Allocation_Info"]
-EXPECTED_PANDAS_DFRAME = pandas.DataFrame([{u'repo_type': u'STR', u'repo_id': 45955, u'user_id': 72048, u'uid': 72048, u'resource_id': 1006, u'user_limit': 202000, u'rname': u'm2015', u'uname': u'timm', u'remain': 201943.31, u'user_percentage': 100, u'user_amount_charged': 0, u'unit_scaling': 1, u'measurement_units': u'SRU', u'rid': 61512, u'fixed': u'N', u'allocated': 202000, u'amount_charged': 56.69}, {u'repo_type': u'STR', u'repo_id': 55555, u'user_id': 72048, u'uid': 72048, u'resource_id': 1006, u'user_limit': 1000, u'rname': u'm2696', u'uname': u'timm', u'remain': 1000, u'user_percentage': 100, u'user_amount_charged': 0, u'unit_scaling': 1, u'measurement_units': u'SRU', u'rid': 72536, u'fixed': u'N', u'allocated': 1000, u'amount_charged': 0}, {
-                                          u'repo_type': u'STR', u'repo_id': 45955, u'user_id': 72048, u'uid': 72048, u'resource_id': 1006, u'user_limit': 202000, u'rname': u'm2015', u'uname': u'timm', u'remain': 201943.31, u'user_percentage': 100, u'user_amount_charged': 0, u'unit_scaling': 1, u'measurement_units': u'SRU', u'rid': 61512, u'fixed': u'N', u'allocated': 202000, u'amount_charged': 56.69}, {u'repo_type': u'STR', u'repo_id': 55555, u'user_id': 72048, u'uid': 72048, u'resource_id': 1006, u'user_limit': 1000, u'rname': u'm2696', u'uname': u'timm', u'remain': 1000, u'user_percentage': 100, u'user_amount_charged': 0, u'unit_scaling': 1, u'measurement_units': u'SRU', u'rid': 72536, u'fixed': u'N', u'allocated': 1000, u'amount_charged': 0}])
-
+EXPECTED_PANDAS_DFRAME = pandas.DataFrame([{u'uid': 72048, u'firstname': u'Steven', u'middlename': u'C', u'projectId': 54807, u'currentAlloc': 374400000000.0, u'userAlloc': 0.0, u'repoType': u'REPO', u'repoName': u'm2612', u'lastname': u'Timm', u'userAllocPct': 2.0, u'usedAlloc': 560.0, u'name': u'timm'}])
 
 class TestNerscAllocationInfo:
 
@@ -35,9 +34,15 @@ class TestNerscAllocationInfo:
         assert nersc_allocations.produces() == PRODUCES
 
     def test_acquire(self):
+
+        def side_effect_get_usage(username):
+            if username == FAKE_USER:
+                return {'items': []}
+            return utils.input_from_file(ALLOCATIONS_FIXTURE_FILE)
+
         nersc_allocations = NerscAllocationInfo.NerscAllocationInfo(CONFIG)
         with mock.patch.object(newt.Newt, "get_usage") as f:
-            f.return_value = utils.input_from_file(ALLOCATIONS_FIXTURE_FILE)
+            f.side_effect = side_effect_get_usage
             res = nersc_allocations.acquire()
             assert PRODUCES == list(res.keys())
             assert EXPECTED_PANDAS_DFRAME.equals(res[PRODUCES[0]])
