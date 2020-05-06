d16e0dbc7bb83aa3cf2bed9a3589569afe5a2c06
root <root@oasistestgpvm2.fnal.gov>
2020-05-05 19:28:32 -0500

    retry_on_error implemented as a util pep8 corrected

--- util/retry_function.py
+++ util/retry_function.py
@@ -2,17 +2,17 @@ from functools import wraps
 import logging
 import time
 
-def retry_on_error( nretries=1 ):
+def retry_on_error(nretries=1):
     def decorator(f):
         @wraps(f)
         def wrapper(*args, **kwargs):
             time2sleep = 0
             logger = logging.getLogger()
-            for i in range(nretries+1):
+            for i in range(nretries + 1):
                 try:
                     if i != 0:
-                        time2sleep = 2**(i-1)
-                    time.sleep( time2sleep )
+                        time2sleep = 2**(i - 1)
+                    time.sleep(time2sleep)
                     return f(*args, **kwargs)
                 except Exception as e:
                     logger.warning("Error Function {0:s} failed with {1:s} on try {2:d}/{3:d}".format(f.__name__, e, i, nretries))
