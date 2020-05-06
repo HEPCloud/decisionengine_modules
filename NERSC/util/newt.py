e055f8d50dc7119b086192e219584e9e59a80ced
sbhat <sbhat@fnal.gov>
2020-05-06 14:20:21 -0500

    Import retry from urllib3 directly

--- NERSC/util/newt.py
+++ NERSC/util/newt.py
@@ -7,7 +7,7 @@ import os
 import time
 import requests
 from requests.adapters import HTTPAdapter
-from requests.packages.urllib3.util.retry import Retry
+from urllib3.util.retry import Retry
 
 """
 Newt base URL. When not specified in constructor this
