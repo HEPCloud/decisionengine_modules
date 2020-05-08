c6685c8d2600b300a82aae680366806ec6635d79
root <root@oasistestgpvm2.fnal.gov>
2020-05-08 15:08:19 -0500

    Simplifying sleep algorithm

--- util/retry_function.py
+++ util/retry_function.py
@@ -12,21 +12,18 @@ def retry_on_error(nretries=1, retry_interval=2):
             if hasattr(localself, 'retry_interval'):
                 retry_interval = localself.retry_interval
 
-            time2sleep = 0
+            time2sleep = 1
             logger = logging.getLogger()
             for i in range(nretries + 1):
                 try:
-                    if i == 1:
-                        time2sleep = 1
-                    elif i > 1:
-                        time2sleep *= retry_interval
-                    time.sleep(time2sleep)
                     return f(*args, **kwargs)
                 except Exception as e:
-                    logger.warning("Error Function {:s} failed with {:s} on try {:d}/{:d}".format(f.__name__, e, i, nretries))
                     if i == nretries:
-                        logger.error("Error Function {:s} failed with {:s} after {:d} retry".format(f.__name__, e, i))
+                        logger.error("Error Function {:s} giving up with {:s} after {:d} retry".format(f.__name__, e, i))
                         raise e
+                    logger.warning("Function {:s} failed with {:s} on try {:d}/{:d}. Sleeping {:d} seconds".format(f.__name__, e, i, nretries,time2sleep))
+                    time.sleep(time2sleep)
+                    time2sleep *= retry_interval
 
         return wrapper
     return decorator
