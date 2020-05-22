e6efd9bf84c2c7ab4baf23149251d29b5b5317d0
Dmitry O Litvintsev <dmitrylitvintsev@mac-131313.dhcp.fnal.gov>
2020-05-22 14:31:59 -0500

    Fix pep8 warnings

--- AWS/sources/BillingInfo.py
+++ AWS/sources/BillingInfo.py
@@ -1,20 +1,18 @@
-import six
-import zipfile
-import csv
-import io
-import string
-import re
-import datetime
-import time
-import sys
-import os
 import copy
-import pprint
-import boto3
-from boto3.session import Session
-import numpy as np
-import pandas as pd
+import csv
+import datetime
+import io
 import logging
+import os
+import pprint
+import re
+import string
+import time
+import zipfile
+
+import boto3
+import pandas as pd
+from boto3.session import Session
 
 from decisionengine.framework.modules import Source
 from decisionengine_modules.AWS.sources import DEAccountContants
@@ -421,10 +419,6 @@ class AWSBillCalculator(object):
             billingFileNameNewFormatIdentifier)
         billingFileDateIdentifier = r'20[0-9][0-9]\-[0-9][0-9]'
         dateExtractionMatch = re.compile(billingFileDateIdentifier)
-        newLastColumnHeaderString = 'ResourceId'
-        new5thColumnHeaderString = 'RecordId'
-        old4thColumnHeaderString = 'RecordType'
-        newFormat = True
         data_by_month = {}
         for zipFileName in zipFileList:
             dateMatch = dateExtractionMatch.search(zipFileName)
@@ -433,11 +427,6 @@ class AWSBillCalculator(object):
                     'Cannot identify date in billing file name %s' % (zipFileName,))
             date_key = dateMatch.group(0)
             data_by_month[date_key] = ''
-        # Check if file is in new or old format
-        if billingFileNameNewFormatMatch.search(zipFileName) is None:
-            newFormat = False
-        else:
-            newFormat = True
 
         # Read in files for the merging
         zipFile = zipfile.ZipFile(zipFileName, 'r')
@@ -671,7 +660,6 @@ class BillingInfo(Source.Source):
         """
 
         # get data for all accounts
-        d = {}
         data = []
         datarate = []
         for i in self.accounts:
@@ -758,7 +746,7 @@ class BillingInfo(Source.Source):
 
             except Exception as detail:
                 print(detail)
-            except Exception as e:
+            except Exception:
                 pass
 
         return {PRODUCES[0]: pd.DataFrame(data), PRODUCES[1]: pd.DataFrame(datarate)}
