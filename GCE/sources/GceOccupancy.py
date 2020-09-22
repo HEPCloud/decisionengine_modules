fada4725221bbea5eb0afa0dbd92eada2475f960
Pat Riehecky <riehecky@fnal.gov>
2020-09-22 10:13:27 -0500

    Correct 'file_cache is unavailable when using oauth2client >= 4.0.0'
    RB: https://fermicloud140.fnal.gov/reviews/r/220/

--- GCE/sources/GceOccupancy.py
+++ GCE/sources/GceOccupancy.py
@@ -9,6 +9,11 @@ import pandas as pd
 import google.auth
 import googleapiclient.discovery
 
+# https://github.com/googleapis/google-api-python-client/issues/299#issuecomment-268915510
+# check if our oauth2client supports cache_discovery or not
+import oauth2client
+from packaging import version
+
 from decisionengine.framework.modules import Source
 from decisionengine_modules.util.retry_function import retry_wrapper
 
@@ -24,8 +29,11 @@ class GceOccupancy(Source.Source):
         super(GceOccupancy, self).__init__(config)
         os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = config["credential"]
         credentials, self.project = google.auth.default()
+
+        use_cache = version.parse(oauth2client.__version__) < version.parse('4.0.0')
         self.client = googleapiclient.discovery.build(
-            "compute", "v1", credentials=credentials)
+            "compute", "v1", credentials=credentials,
+            cache_discovery=use_cache)
         self.max_retries = config.get("max_retries", _MAX_RETRIES)
         self.retry_timeout = config.get("retry_timeout", _RETRY_TIMEOUT)
 
