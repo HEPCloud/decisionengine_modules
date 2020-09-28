1202029d55a641aa625e56017626c95cd58af896
Marco Mambelli <marcom@fnal.gov>
2020-09-28 12:52:47 -0500

    Added debugging for m2crypto

--- .github/actions/unittest-in-sl7-docker/entrypoint.sh
+++ .github/actions/unittest-in-sl7-docker/entrypoint.sh
@@ -13,6 +13,12 @@ make liblinks
 cd -
 export PYTHONPATH=$PWD:$PYTHONPATH
 source venv/bin/activate
+# MMDB
+echo "MMDB Python (`which python`): `python --version`"
+echo "MMDB M2Crypto: `yum list *m2crypto*`"
+echo "MMDB M2Crypto python: `python3 -c "import M2Crypto; print (M2Crypto.__path__)"`"
+echo "MMDB M2Crypto python3: `python3 -c "import M2Crypto; print (M2Crypto.__path__)"`"
+
 pytest -v -l --tb=native decisionengine_modules > pytest.log 2>&1
 status=$?
 cat pytest.log
