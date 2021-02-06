cd7e66810d8eadd7a8331ad1b006a638f342b325
Pat Riehecky <3534830+jcpunk@users.noreply.github.com>
2021-02-05 21:24:44 -0600

    Move to raw github actions (and codecov.io) (#298)
    RB : https://fermicloud140.fnal.gov/reviews/r/320/

--- AWS/tests/test_AWS_figure_of_merit_publisher.py
+++ AWS/tests/test_AWS_figure_of_merit_publisher.py
@@ -6,16 +6,30 @@ import pandas as pd
 import decisionengine_modules.AWS.publishers.AWS_figure_of_merit as AWSFOMPublisher
 
 DATA_DIR = os.path.join(os.path.dirname(__file__), "data")
-EXPECTED_REPLY = pd.read_csv(os.path.join(DATA_DIR,
-                                          "expected_AWS_figure_of_merit_pub.csv"))
+
+try:
+    # pandas 1.2 uses a new method for floats, use the "legacy" one for now
+    # https://pandas.pydata.org/pandas-docs/stable/whatsnew/v1.2.0.html#change-in-default-floating-precision-for-read-csv-and-read-table
+    EXPECTED_REPLY = pd.read_csv(os.path.join(DATA_DIR,
+                                              "expected_AWS_figure_of_merit_pub.csv"), float_precision='legacy')
+except TypeError:
+    EXPECTED_REPLY = pd.read_csv(os.path.join(DATA_DIR,
+                                              "expected_AWS_figure_of_merit_pub.csv"))
 OUTPUT_FILE = "AWS_figure_of_merit_pub.csv"
 CONSUMES = ["AWS_Figure_Of_Merit"]
 
 
 def create_datablock():
     data_block = {}
-    data_block['AWS_Figure_Of_Merit'] = pd.read_csv(os.path.join(DATA_DIR,
-                                                                 'expected_figure_of_merit.csv'))
+    try:
+        # pandas 1.2 uses a new method for floats, use the "legacy" one for now
+        # https://pandas.pydata.org/pandas-docs/stable/whatsnew/v1.2.0.html#change-in-default-floating-precision-for-read-csv-and-read-table
+        data_block['AWS_Figure_Of_Merit'] = pd.read_csv(os.path.join(DATA_DIR,
+                                                                     'expected_figure_of_merit.csv'),
+                                                        float_precision='legacy')
+    except TypeError:
+        data_block['AWS_Figure_Of_Merit'] = pd.read_csv(os.path.join(DATA_DIR,
+                                                                     'expected_figure_of_merit.csv'))
     return data_block
 
 
@@ -40,5 +54,11 @@ class TestAWSFOMPublisher(unittest.TestCase):
     def test_transform(self):
         data_block = create_datablock()
         self.publisher.publish(data_block)
-        opd = pd.read_csv(OUTPUT_FILE)
-        self.assertTrue(opd.equals(EXPECTED_REPLY))
+        try:
+            # pandas 1.2 uses a new method for floats, use the "legacy" one for now
+            # https://pandas.pydata.org/pandas-docs/stable/whatsnew/v1.2.0.html#change-in-default-floating-precision-for-read-csv-and-read-table
+            opd = pd.read_csv(OUTPUT_FILE, float_precision='legacy')
+        except TypeError:
+            opd = pd.read_csv(OUTPUT_FILE)
+
+        self.assertEqual(opd.to_csv(), EXPECTED_REPLY.to_csv())
