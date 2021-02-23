6285f49b051784cb788fe20629ad7b9a8547db2e
Pat Riehecky <3534830+jcpunk@users.noreply.github.com>
2021-02-23 12:39:00 -0600

    Drop obsolete github action (#309)
    RB : https://fermicloud140.fnal.gov/reviews/r/347/

--- .github/actions/pep8-in-sl7-docker/entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-PYVER=${1:-"3.6"}
-GITHUB_WORKSPACE=${GITHUB_WORKSPACE:-`pwd`}
-env PYVER=$PYVER decisionengine_modules/build/scripts/run_pep8.sh
-tar cvfj $GITHUB_WORKSPACE/logs.tar.bz2 pep8.*.log
-cat pep8.*.log
-exit `cat pep8*.log | wc -l`
