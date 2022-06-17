a76456276c7737a33fe51dba2645ee4f3c7518c9
Kyle Knoepfel <knoepfel@fnal.gov>
2022-06-17 16:12:38 +0000

    Silence warnings that are beyond our control or are irrelevant.

--- src/decisionengine_modules/glideinwms/tests/test_configure_gwms_frontend.py
+++ src/decisionengine_modules/glideinwms/tests/test_configure_gwms_frontend.py
@@ -1,10 +1,17 @@
 # SPDX-FileCopyrightText: 2017 Fermi Research Alliance, LLC
 # SPDX-License-Identifier: Apache-2.0
 
+import warnings
+
 from argparse import ArgumentParser
 
-from decisionengine_modules.glideinwms import configure_gwms_frontend
 from decisionengine_modules.glideinwms.tests.fixtures import de_client_config  # noqa: F401
+
+# If using HTCondor without a configuration becomes important, we can
+# put this warning filter in the pyproject.toml file.
+with warnings.catch_warnings():
+    warnings.filterwarnings("ignore", category=UserWarning, module="htcondor")
+    from decisionengine_modules.glideinwms import configure_gwms_frontend
 
 
 def test_get_arg_parser():
