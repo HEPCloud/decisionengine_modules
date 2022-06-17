97e03e653113867b366ec8d21a7d27a997d14a24
Kyle Knoepfel <knoepfel@fnal.gov>
2022-06-17 13:22:30 +0000

    Fix NERSC FOM test.

--- src/decisionengine_modules/tests/test_NerscFigureOfMerit.py
+++ src/decisionengine_modules/tests/test_NerscFigureOfMerit.py
@@ -7,36 +7,30 @@ import pytest
 from decisionengine.framework.modules.Module import verify_products
 from decisionengine_modules.NERSC.transforms import NerscFigureOfMerit
 
-nersc_instance_performance_df = pd.DataFrame(
-    [
-        {
-            "EntryName": "CMSHTPC_T3_US_NERSC_Cori",
-            "InstanceType": "haswell",
-            "AvailabilityZone": "cori",
-            "OnDemandPrice": 0.576,
-            "PerfTtbarTotal": 0.96,
-        }
-    ]
-).reindex(columns=("EntryName", "InstanceType", "AvailabilityZone", "OnDemandPrice", "PerfTtbarTotal"))
-
 data_block = {
-    "Nersc_Instance_Performance": nersc_instance_performance_df.reindex(
-        columns=("EntryName", "InstanceType", "AvailabilityZone", "OnDemandPrice", "PerfTtbarTotal")
+    "Nersc_Instance_Performance": pd.DataFrame(
+        [
+            {
+                "EntryName": "CMSHTPC_T3_US_NERSC_Cori",
+                "InstanceType": "haswell",
+                "AvailabilityZone": "cori",
+                "OnDemandPrice": 0.576,
+                "PerfTtbarTotal": 0.96,
+            }
+        ]
     ),
     "Factory_Entries_LCF": pd.DataFrame(
         [
             {
                 "EntryName": "CMSHTPC_T3_US_NERSC_Cori",
                 "GlideinConfigPerEntryMaxGlideins": 200,
-                "GlideinMonitorTotalStatusRunning": 100,
+                "GlideinMonitorTotalStatusRunning": 99,
                 "GlideinConfigPerEntryMaxIdle": 10,
-                "GlideinMonitorTotalStatusIdle": 10,
+                "GlideinMonitorTotalStatusIdle": 9,
             }
         ]
     ),
 }
-
-nersc_price_performance_df = pd.DataFrame([{"EntryName": "CMSHTPC_T3_US_NERSC_Cori", "PricePerformance": 0.6}])
 
 
 @pytest.fixture
@@ -51,13 +45,10 @@ def test_produces(nersc_figure_of_merit):
 
 def test_transform(nersc_figure_of_merit):
     expected_transform_output = {
-        "Nersc_Price_Performance": nersc_price_performance_df.reindex(columns=("EntryName", "PricePerformance")),
+        "Nersc_Price_Performance": pd.DataFrame([{"EntryName": "CMSHTPC_T3_US_NERSC_Cori", "PricePerformance": 0.6}]),
         "Nersc_Figure_Of_Merit": pd.DataFrame([{"EntryName": "CMSHTPC_T3_US_NERSC_Cori", "FigureOfMerit": 0.3}]),
     }
     res = nersc_figure_of_merit.transform(data_block)
     verify_products(nersc_figure_of_merit, res)
     for key, value in res.items():
-        try:
-            assert expected_transform_output[key].equals(value)
-        except Exception:
-            print(key, " fail\n", expected_transform_output[key], "\n", value)
+        assert expected_transform_output[key].equals(value)
