60b8b229cc602c102077c86d5d56d339902a8415
Enes Erkoc <mee57@miami.edu>
2022-09-08 19:07:01 +0000

    Renamed Variables (REQ_IDLE_GLIDEINS & REQ_MAX_GLIDEINS) and stored them into a global dictionary.

--- src/decisionengine_modules/glideinwms/transforms/glidein_requests.py
+++ src/decisionengine_modules/glideinwms/transforms/glidein_requests.py
@@ -113,8 +113,8 @@ class GlideinRequestManifests(Transform.Transform):
             # Get the jobs dataframe
             jobs_df = self.job_manifests(datablock)
             METRICS["NUMBER_OF_JOBS"].set(jobs_df.shape[0])
-            # NAME_OF_GROUP.group_manifests(GroupName)
-            # STATUS_OF_JOB.(jobs_df.JobStatus)
+           # METRICS["NAME_OF_GROUP"].group_manifests(GroupName)
+           # METRICS["STATUS_OF_JOB"].(jobs_df.JobStatus)
 
             # Get the job clusters dataframe
             job_clusters_df = self.job_clusters(datablock)
@@ -160,14 +160,14 @@ class GlideinRequestManifests(Transform.Transform):
                 ]
                 req_idle_glideins_per_ce = {ce: count for ce, count in zip(entry_ces, req_idle_glideins)}
                 for ce, count in req_idle_glideins_per_ce.items():
-                    REQ_IDLE_GLIDEINS.labels(ce).set(count)
+                    METRICS["REQ_IDLE_GLIDEINS"].labels(ce).set(count)
 
                 req_max_glideins = [
                     ce_row.ReqMaxGlideins for ce_row in group_manifests["glideclient_manifests"].itertuples()
                 ]
                 req_max_glideins_per_ce = {ce: count for ce, count in zip(entry_ces, req_max_glideins)}
                 for ce, count in req_max_glideins_per_ce.items():
-                    REQ_MAX_GLIDEINS.labels(ce).set(count)
+                    METRICS["REQ_MAX_GLIDEINS"].labels(ce).set(count)
 
                 manifests = self.merge_requests(manifests, group_manifests)
         except Exception:
