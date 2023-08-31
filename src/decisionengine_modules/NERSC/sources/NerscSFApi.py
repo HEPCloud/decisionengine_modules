f02005692abea1049f201dac92f02603c78e959c
root <root@fermicloud850.fnal.gov>
2023-08-31 19:53:07 +0000

    New Nersc Source Module using SuperFacilityApi, First a few lines added

--- src/decisionengine_modules/NERSC/sources/NerscSFApi.py
+++ src/decisionengine_modules/NERSC/sources/NerscSFApi.py
@@ -1,3 +1,9 @@
+# SPDX-FileCopyrightText: 2017 Fermi Research Alliance, LLC
+# SPDX-License-Identifier: Apache-2.0
+
+"""
+Get allocation info from Nersc SuperFacilityAPI
+"""
 import pandas as pd
 from decisionengine.framework.modules import Source
 from decisionengine.framework.modules.Source import Parameter
