9980e3a6974d2339c8412c83868596ab17a5ec1c
Vito Di Benedetto <55766483+vitodb@users.noreply.github.com>
2024-02-01 18:42:35 -0600

    Make the test flake8 compliant

--- src/decisionengine_modules/glideinwms/tests/test_DEConfigSource.py
+++ src/decisionengine_modules/glideinwms/tests/test_DEConfigSource.py
@@ -34,7 +34,7 @@ def test_instantiation(tmp_path):  # noqa: F811
     config_file = tmp_path / "config.json"
     config_file.write_text(GOOD_CONFIG)
     config = DEConfigSource(config_file)
-    assert type(config) == DEConfigSource
+    assert isinstance(config, DEConfigSource)
     assert config["value"] == "foo"
     assert config["list"] == [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
     assert config["dict"] == {"a": 1, "b": 2, "c": 3}
