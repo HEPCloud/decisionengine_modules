b024414754d4e8715592ea67c46f2df97f5cf186
Skyler Foster <skylerfoster51@gmail.com>
2024-03-20 15:59:17 -0500

    Fixed new figure of merit metric and instrumentation in 'grid_figure_of_merit.py'

--- src/decisionengine_modules/glideinwms/transforms/grid_figure_of_merit.py
+++ src/decisionengine_modules/glideinwms/transforms/grid_figure_of_merit.py
@@ -6,10 +6,10 @@ import pandas
 from decisionengine.framework.modules import Transform
 from decisionengine.framework.modules.Transform import Parameter
 from decisionengine_modules.util.figure_of_merit import figure_of_merit
+from decisionengine_modules.util.figure_of_merit import FIGURE_OF_MERIT_CALCULATION
 
 ATTR_ENTRYNAME = "EntryName"
 ATTR_FOM = "Grid_Figure_Of_Merit"
-
 
 @Transform.supports_config(Parameter("price_performance", default=1))
 @Transform.consumes(Factory_Entries=pandas.DataFrame)
@@ -37,24 +37,19 @@ class GridFigureOfMerit(Transform.Transform):
                 max_allowed = float(entry["GlideinConfigPerEntryMaxGlideins"])
                 max_idle = float(entry["GlideinConfigPerEntryMaxIdle"])
                 idle = float(entry["GlideinMonitorTotalStatusIdle"])
-               
+
+                # Instrumentation
                 fom_value = figure_of_merit(
                         self.price_performance, running, max_allowed, idle, max_idle, self.logger
                 )
-
                 f = {
                     ATTR_ENTRYNAME: entry[ATTR_ENTRYNAME],
                     ATTR_FOM: fom_value
                 }
                 foms.append(f)
 
-                FIGURE_OF_MERIT_CALCULATION.labels(
-                        performance=self.price_performance,
-                        running=running,
-                        allowed=max_allowed,
-                        idle=idle,
-                        max_idle=max_idle
-                ).set(fom_value)
+                # FOM Metric 
+                FIGURE_OF_MERIT_CALCULATION.labels(performance=self.price_performance, running=running, allowed=max_allowed, idle=idle, max_idle=max_idle).set(fom_value)
 
         return {"Grid_Figure_Of_Merit": pandas.DataFrame(foms)}
 
