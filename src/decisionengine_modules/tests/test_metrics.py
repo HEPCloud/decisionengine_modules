0c51710112575d24269980f078e50c1e859d8b99
IlyaBaburashvili <ilyab3@yahoo.com>
2024-03-19 19:43:38 -0500

    Added loading expected values from json

--- src/decisionengine_modules/tests/test_metrics.py
+++ src/decisionengine_modules/tests/test_metrics.py
@@ -1,6 +1,7 @@
 # SPDX-FileCopyrightText: 2017 Fermi Research Alliance, LLC
 # SPDX-License-Identifier: Apache-2.0
 
+import json
 import os
 
 from unittest import mock
@@ -9,7 +10,7 @@ import pandas
 import pytest
 
 from decisionengine_modules.htcondor import htcondor_query
-from decisionengine_modules.htcondor.sources import slots, source
+from decisionengine_modules.htcondor.sources import source
 from decisionengine_modules.htcondor.sources.source import (
     DEM_HTCONDOR_CORES_COUNT,
     DEM_HTCONDOR_MEMORY_COUNT,
@@ -19,6 +20,7 @@ from decisionengine_modules.util import testutils as utils
 
 DATA_DIR = os.path.join(os.path.dirname(__file__), "data")
 FIXTURE_FILE = os.path.join(DATA_DIR, "cs.fixture")
+EXPECTED_VALUES_FILE = os.path.join(DATA_DIR, "expected_values.json")
 
 
 @pytest.fixture
@@ -40,8 +42,14 @@ def setup_metrics_dir_for_test(monkeypatch, tmp_path):
     monkeypatch.setenv("PROMETHEUS_MULTIPROC_DIR", tmp_path)
 
 
-@pytest.mark.unit
-def test_count_slots(source_instance, setup_metrics_dir_for_test, expected_values):
+def load_expected_values():
+    with open(EXPECTED_VALUES_FILE) as f:
+        return json.load(f)
+
+
+# @pytest.mark.unit
+def test_count_slots(source_instance, setup_metrics_dir_for_test):
+    expected_values = load_expected_values()
     with mock.patch.object(htcondor_query.CondorStatus, "fetch", return_value=utils.input_from_file(FIXTURE_FILE)):
         source_instance.load()
         for label, expected_value in expected_values["Slots"].items():
@@ -49,8 +57,9 @@ def test_count_slots(source_instance, setup_metrics_dir_for_test, expected_value
             assert metric_value == expected_value
 
 
-@pytest.mark.unit
-def test_count_cores(source_instance, setup_metrics_dir_for_test, expected_values):
+# @pytest.mark.unit
+def test_count_cores(source_instance, setup_metrics_dir_for_test):
+    expected_values = load_expected_values()
     with mock.patch.object(htcondor_query.CondorStatus, "fetch", return_value=utils.input_from_file(FIXTURE_FILE)):
         source_instance.load()
         for label, expected_value in expected_values["Cores"].items():
@@ -58,8 +67,9 @@ def test_count_cores(source_instance, setup_metrics_dir_for_test, expected_value
             assert metric_value == expected_value
 
 
-@pytest.mark.unit
-def test_count_memory(source_instance, setup_metrics_dir_for_test, expected_values):
+# @pytest.mark.unit
+def test_count_memory(source_instance, setup_metrics_dir_for_test):
+    expected_values = load_expected_values()
     with mock.patch.object(htcondor_query.CondorStatus, "fetch", return_value=utils.input_from_file(FIXTURE_FILE)):
         source_instance.load()
         for label, expected_value in expected_values["Memory"].items():
