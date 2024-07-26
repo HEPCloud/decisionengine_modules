a85edeb7b2f622eaf32915613200558eb15c41fa
Namratha Urs <namrathaurs@my.unt.edu>
2024-07-26 15:52:07 -0500

    migrating to using bigquery for GCE billing

--- src/decisionengine_modules/GCE/sources/GCEBillingInfo.py
+++ src/decisionengine_modules/GCE/sources/GCEBillingInfo.py
@@ -1,23 +1,21 @@
 # SPDX-FileCopyrightText: 2017 Fermi Research Alliance, LLC
 # SPDX-License-Identifier: Apache-2.0
 
+# standard library imports
 import pandas as pd
-
+# related third party imports
 from bill_calculator_hep.GCEBillAnalysis import GCEBillCalculator
-
+# local application/library specific imports
 from decisionengine.framework.modules import Source
 from decisionengine.framework.modules.Source import Parameter
 
 
 @Source.supports_config(
     Parameter("projectId", type=str),
-    Parameter("credentialsProfileName", type=str, comment="not currently used"),
-    Parameter("accountNumber", type=int, comment="not currently used"),
     Parameter("lastKnownBillDate", type=str, comment="in the form '%m/%d/%y %H:%M'"),
-    Parameter("balanceAtData", type=float, comment="in dollars"),
+    Parameter("balanceAtDate", type=float, comment="in dollars"),
     Parameter("applyDiscount", type=bool, comment="DLT discount does not apply to credits"),
-    Parameter("botoConfig", comment="not currently used"),
-    Parameter("localFileDir", type=dir, comment="location for downloaded billing files"),
+    Parameter("sumToDate", type=str, comment="in the form '%m/%d/%y %H:%M'"),
 )
 @Source.produces(GCE_Billing_Info=pd.DataFrame)
 class GCEBillingInfo(Source.Source):
@@ -25,15 +23,11 @@ class GCEBillingInfo(Source.Source):
         super().__init__(config)
         # Load configuration "constants"
         self.projectId = config.get("projectId")
-        self.credentialsProfileName = config.get("credentialsProfileName")  # NOT CURRENTLY USED
-        self.accountNumber = config.get("accountNumber")  # NOT CURRENTLY USED
         self.lastKnownBillDate = config.get("lastKnownBillDate")  # '%m/%d/%y %H:%M'
         self.balanceAtDate = config.get("balanceAtDate")  # $
         # Onix does not provide discounts
         self.applyDiscount = config.get("applyDiscount")
-        self.botoConfig = config.get("botoConfig")  # BOTO_CONFIG env
-        # location for downloaded billing files
-        self.localFileDir = config.get("localFileDir")
+        self.sumToDate = config.get("sumToDate")
 
     def acquire(self):
         """
@@ -44,26 +38,25 @@ class GCEBillingInfo(Source.Source):
         self.logger.debug("in GCEBillingInfo acquire")
         constantsDict = {
             "projectId": self.projectId,
-            "credentialsProfileName": self.credentialsProfileName,
-            "accountNumber": self.accountNumber,
-            "bucketBillingName": "billing-" + str(self.projectId),
             "lastKnownBillDate": self.lastKnownBillDate,
             "balanceAtDate": self.balanceAtDate,
             "applyDiscount": self.applyDiscount,
+            "sumToDate": self.sumToDate
         }
-        globalConf = {"graphite_host": "dummy", "graphite_context_billing": "dummy", "outputPath": self.localFileDir}
+        globalConf = {"graphite_host": "dummy", "graphite_context_billing": "dummy"}
+        bill_summary = pd.DataFrame()
         try:
             calculator = GCEBillCalculator(None, globalConf, constantsDict, self.logger)
 
-            lastStartDateBilledConsideredDatetime, CorrectedBillSummaryDict = calculator.CalculateBill()
+            bill_summary = calculator.CalculateBill()
 
-            self.logger.info("Calculated corrected bill summary for google")
-            self.logger.info(CorrectedBillSummaryDict)
+            self.logger.info("Calculated corrected bill summary for Google (using BigQuery)")
+            self.logger.debug(bill_summary)
 
         except Exception:
             self.logger.exception("Exception in GCEBillingInfo call to acquire")
 
-        return {"GCE_Billing_Info": pd.DataFrame([CorrectedBillSummaryDict])}
+        return {"GCE_Billing_Info": bill_summary}
 
 
 Source.describe(GCEBillingInfo)
