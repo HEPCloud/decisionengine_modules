6a1537bbdc1faa20607821934778aed77286b23a
Namratha Urs <namrathaurs@my.unt.edu>
2025-03-05 17:02:02 -0600

    removed test for billing files download

--- src/decisionengine_modules/tests/test_GCEBillingInfo.py
+++ src/decisionengine_modules/tests/test_GCEBillingInfo.py
@@ -33,21 +33,3 @@ config_billing_info = {
 def test_produces():
     bi_pub = GCEBillingInfo.GCEBillingInfo(config_billing_info)
     assert bi_pub._produces == {"GCE_Billing_Info": pandas.DataFrame}
-
-
-def test_unable_to_download_filelist():
-    constantsDict = {
-        "projectId": "hepcloud-fnal",
-        "credentialsProfileName": "BillingBlah",
-        "accountNumber": 1111,
-        "bucketBillingName": "billing-hepcloud-fnal",
-        "lastKnownBillDate": "10/01/18 00:00",
-        "balanceAtDate": 100.0,
-        "applyDiscount": True,
-    }
-    globalConf = {"graphite_host": "dummy", "graphite_context_billing": "dummy", "outputPath": "."}
-
-    calculator = GCEBillCalculator(None, globalConf, constantsDict, structlog.getLogger())
-
-    file_list = calculator._downloadBillFiles()
-    assert file_list == []
